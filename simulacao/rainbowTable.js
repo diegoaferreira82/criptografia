e868da8b29e3e8028e842b2172408177a68f37b9
diegoaferreira82 <diegoaferreira82@hotmail.com>
2026-02-25 17:16:08 -0400

    final

--- simulacao/rainbowTable.js
+++ simulacao/rainbowTable.js
@@ -14,7 +14,6 @@ const hashesVazadas = ["21232f297a57a5a743894a0e4a801fc3",
 "cedf5ab7b5c5852b3ed35d7dbe3c3835",
 "81dc9bdb52d04dc20036dbd8313ed055"]
 
-//
 hashesVazadas.map(hashVazada => {
     rainbowTable.map(parGerado => {
         if (hashVazada === parGerado[1]) {
